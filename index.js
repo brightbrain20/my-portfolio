ae5f2087f76307d353ea6294c2c41545249e4781
brightbrain20 <chibuzorbrightezeamaka@gmail.com>
2023-03-07 09:49:04 -0800

    modify html and js file

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const removeBtn = document.querySelector('.remove-btn');
 const mobileNav = document.querySelector('#mobile-navigation');
 const mobileNavLinks = document.querySelectorAll('.mobile-item');
 const popupContainer = document.querySelector('.popup');
+const work = document.querySelector('.work-container');
+const about = document.querySelector('#about');
+
 
 const navToggle = () => {
   mobileNav.classList.toggle('active');
@@ -62,6 +65,175 @@ const displayPopup = (id) => {
   });
 };
 
+function populateProjets() {
+  about.innerHTML =` 
+  <div class="Recent-work">
+      <div class="Recent">My Recent Works</div>
+      <div class="hr">
+        <hr>
+      </div>
+    </div>
+    <div class="Multi-Post-container">
+      <div class="about-img">
+        <img src="./Images/Img Placeholder.png" alt="">
+      </div>
+      <div class="desktop">
+        <img src="./Images/Img Placeholder (2).png" class="img-about" alt="">
+      </div>
+      <div class="Multi">
+        <p class="Multi-Post-Heading">
+          Multi-Post Stories
+        </p>
+        <p class="Multi-Post-stories">
+          A daily selection of privately personalized reads; no accounts or sign-ups required.
+          has been the industry's standard dummy text ever since the 1500s, when an unknown
+          printer took a standard dummy text.
+        </p>
+        <ul class="Multi-Post-ul">
+          <li class="Multi-Post-li">
+            css
+          </li>
+          <li class="Multi-Post-li">
+            html
+          </li>
+          <li class="Multi-Post-li bootstrap">
+            bootstrap
+          </li>
+          <li class="Multi-Post-li">
+            Ruby
+          </li>
+        </ul>
+        <a href="#1" id="one"  class="Multi-Post-project cards button">See Project</a>
+      </div>
+    </div>
+  `
+  work.innerHTML = `
+  <div class="art-container">
+  <div class="heading">
+    <div class="heading-body">
+      <p class="profesional-Art-heading">
+        Profesional Art Printing Data
+      </p>
+      <p class="profesional-Art-content">
+        A daily
+        selection of privately personalized reads;
+        no accounts or sign-ups required. has been the
+        industry's standard
+      </p>
+      <ul class="profesional-Art-ul">
+        <li class="profesional-Art-li">html</li>
+        <li class="profesional-Art-li">bootstrap</li>
+        <li class="profesional-Art-li">Ruby</li>
+      </ul>
+    </div>
+    <div class="heading-btn"><a href="#2" id="two" class="profesional-Art-btm cards button">See Project</a></div>
+  </div>
+  <div class="heading flex2">
+    <div class="heading-body">
+      <p class="profesional-Art-heading">
+        Data Dashboard Healthcare
+      </p>
+      <p class="profesional-Art-content">
+        A daily
+        selection of privately personalized reads;
+        no accounts or sign-ups required. has been the
+        industry's standard
+      </p>
+      <ul class="profesional-Art-ul">
+        <li class="profesional-Art-li">html</li>
+        <li class="profesional-Art-li">bootstrap</li>
+        <li class="profesional-Art-li">Ruby</li>
+      </ul>
+    </div>
+    <div class="heading-btn">
+      <a href="#2" id="three" class="profesional-Art-btm cards button">See Project</a>
+    </div>
+  </div>
+  <div class="heading flex3">
+    <div class="heading-body">
+      <p class="profesional-Art-heading">
+        Data Dashboard Healthcare
+      </p>
+      <p class="profesional-Art-content">
+        A daily
+        selection of privately personalized reads;
+        no accounts or sign-ups required. has been the
+        industry's standard
+      </p>
+      <ul class="profesional-Art-ul">
+        <li class="profesional-Art-li">html</li>
+        <li class="profesional-Art-li">bootstrap</li>
+        <li class="profesional-Art-li">Ruby</li>
+      </ul>
+    </div>
+    <div class="heading-btn"><a href="#2" id="four" class="profesional-Art-btm cards  button">See Project</a></div>
+  </div>
+</div>
+<div class="art-container">
+  <div class="heading">
+    <div class="heading-body">
+      <p class="profesional-Art-heading">
+        Profesional Art Printing Data
+      </p>
+      <p class="profesional-Art-content">
+        A daily
+        selection of privately personalized reads;
+        no accounts or sign-ups required. has been the
+        industry's standard
+      </p>
+      <ul class="profesional-Art-ul">
+        <li class="profesional-Art-li">html</li>
+        <li class="profesional-Art-li">bootstrap</li>
+        <li class="profesional-Art-li">Ruby</li>
+      </ul>
+    </div>
+    <div class="heading-btn"><a href="#2" id="five" class="profesional-Art-btm cards  button">See Project</a></div>
+  </div>
+  <div class="heading flex2">
+    <div class="heading-body">
+      <p class="profesional-Art-heading">
+        Data Dashboard Healthcare
+      </p>
+      <p class="profesional-Art-content">
+        A daily
+        selection of privately personalized reads;
+        no accounts or sign-ups required. has been the
+        industry's standard
+      </p>
+      <ul class="profesional-Art-ul">
+        <li class="profesional-Art-li">html</li>
+        <li class="profesional-Art-li">bootstrap</li>
+        <li class="profesional-Art-li">Ruby</li>
+      </ul>
+    </div>
+    <div class="heading-btn">
+      <a href="#2" id="six" class="profesional-Art-btm cards  button">See Project</a>
+    </div>
+  </div>
+  <div class="heading flex3">
+    <div class="heading-body">
+      <p class="profesional-Art-heading">
+        Data Dashboard Healthcare
+      </p>
+      <p class="profesional-Art-content">
+        A daily
+        selection of privately personalized reads;
+        no accounts or sign-ups required. has been the
+        industry's standard
+      </p>
+      <ul class="profesional-Art-ul">
+        <li class="profesional-Art-li">html</li>
+        <li class="profesional-Art-li">bootstrap</li>
+        <li class="profesional-Art-li">Ruby</li>
+      </ul>
+    </div>
+    <div class="heading-btn"><a href="#2" id="seven" class="profesional-Art-btm cards  button">See Project</a></div>
+  </div>
+</div>
+  `
+ }
+ populateProjets();
+ 
 const cardBtn = document.querySelectorAll('.cards');
 cardBtn.forEach((btn) => {
   btn.addEventListener('click', () => {
@@ -74,33 +246,34 @@ cardBtn.forEach((btn) => {
   });
 });
 
+
 // FORM VALIDATION-------------------
-const form = document.getElementById('getintouch');
-const error = document.getElementById('error');
-const { email } = form.elements;
+// const form = document.getElementById('getintouch');
+// const error = document.getElementById('error');
+// const { email } = form.elements;
 
-function checkUppercase(str) {
-  for (let i = 0; i < str.length; i += 1) {
-    if (str.charAt(i) === str.charAt(i).toUpperCase() && str.charAt(i).match(/[a-z]/i)) {
-      return true;
-    }
-  }
-  return false;
-}
+// function checkUppercase(str) {
+//   for (let i = 0; i < str.length; i += 1) {
+//     if (str.charAt(i) === str.charAt(i).toUpperCase() && str.charAt(i).match(/[a-z]/i)) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
-email.addEventListener('click', () => {
-  error.classList.remove('active');
-  email.classList.remove('active');
-});
+// email.addEventListener('click', () => {
+//   error.classList.remove('active');
+//   email.classList.remove('active');
+// });
 
-form.addEventListener('submit', (event) => {
-  const emailValue = email.value;
-  if (checkUppercase(emailValue)) {
-    event.preventDefault();
-    error.innerHTML = 'please provide a valid email in format: email@email.com';
-    error.classList.add('active');
-    email.classList.add('active');
-  } else {
-    form.submit();
-  }
-});
+// form.addEventListener('submit', (event) => {
+//   const emailValue = email.value;
+//   if (checkUppercase(emailValue)) {
+//     event.preventDefault();
+//     error.innerHTML = 'please provide a valid email in format: email@email.com';
+//     error.classList.add('active');
+//     email.classList.add('active');
+//   } else {
+//     form.submit();
+//   }
+// });
